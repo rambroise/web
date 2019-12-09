02c4597d26319909b7921b868b86ed44fb0cd6bc
rambroise <55062809+rambroise@users.noreply.github.com>
2019-12-09 15:37:14 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function reveal1() {
+    document.getElementById("Answer1").style.display= 'block';
+}
+
+function next1() {
+    document.getElementById("takeaway1").style.display= 'block';
+}
+
+function next2() {
+    document.getElementById("case2").style.display= 'block';
+    document.getElementById("case1").style.display='none';
+}
+
+function next3()
+{
+     document.getElementById("takeaway2").style.display='block';
+}
